4571af0cdf26be78b272f1a34c19c2308bbcd09d
serhii <vasiliev.ck@gmail.com>
2021-08-12 23:59:19 +0300

    fix: update layout

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -103,4 +103,56 @@
             </div>
         </div>
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script>
+        $(function () {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            $('#city').change(function () {
+                let url = '/areas/' + $(this).val();
+                let area = $("#area");
+                if ($(this).val() == 0) {
+                    area.empty();
+                } else {
+                    $.ajax({
+                        type: "GET",
+                        dataType: "json",
+                        url: url,
+                        success: function(data){
+                            area.find('option').remove();
+                            if (Object.keys(data.list).length) {
+                                area.attr('disabled', false);
+                                area.empty();
+                                $.each(data.list, function(index, value) {
+                                    area.append('<option value="'+value+'">'+index+'</option>')
+                                });
+                            } else {
+                                area.attr('disabled', true);
+                            }
+                        },
+                        error: function(jqXHR,textStatus){
+                            alert( "Request failed: " + textStatus);
+                        }
+                    });
+                }
+            });
+            $("#form").click(function () {
+                city = $('#city option:selected').text();
+                area = $('#area option:selected').text();
+                street = $('#street').val();
+                house = $('#house').val();
+                $.getJSON('https://maps.google.com/maps/api/geocode/json?address="Украина,'
+                    +city+','+area+','+street+''+house+'"&language=ru-RU&sensor=false&key=AIzaSyBdyKrHYLdvcYwJ5vur68y5KvGpxCe8XB4',
+                    function(data) {
+                        coor = data.results[0].geometry.location.lat + ',' + data.results[0].geometry.location.lng;
+                        $('#coordinates').val(coor)
+                    });
+            });
+
+        });
+    </script>
 @endsection
